8076b3e732c28e561ae4b13f4219fac6127bb74f
Adam <adamyoung012@gmail.com>
2024-07-09 19:25:11 +1000

    express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('test');
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", async (req, res) => {
+  res.send(`himmet ${process.env.NAME}.`);
+});
+
+app.listen(PORT, async () => {
+  console.log(`App listening on port ${PORT}`);
+});
