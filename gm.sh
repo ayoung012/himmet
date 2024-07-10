bb8b7fd588cd07b8c7384de4f87812e0407e501a
Adam <adamyoung012@gmail.com>
2024-07-10 15:06:38 +1000

    local build graphicsmagick

--- gm.sh
+++ gm.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
-mkdir -p ~/.local
+mkdir -p ~/.local/share
+mkdir ~/.local/include
+mkdir ~/.local/lib
+cat << EOF > ~/local/share/config.site
+CPPFLAGS=-I$HOME/.local/include
+LDFLAGS=-L$HOME/.local/lib
+EOF
+
 cd ~/.local
+wget http://dl-cdn.alpinelinux.org/alpine/v3.19/main/x86_64/freetype-2.13.2-r0.apk;
+wget http://dl-cdn.alpinelinux.org/alpine/v3.19/main/x86_64/libltdl-2.4.7-r3.apk
 wget http://dl-cdn.alpinelinux.org/alpine/v3.19/main/x86_64/libpng-1.6.40-r0.apk
 wget http://dl-cdn.alpinelinux.org/alpine/v3.19/main/x86_64/libxml2-2.11.8-r0.apk
 wget http://dl-cdn.alpinelinux.org/alpine/v3.19/main/x86_64/libjpeg-turbo-3.0.1-r0.apk
-wget http://dl-cdn.alpinelinux.org/alpine/v3.19/community/x86_64/graphicsmagick-1.3.42-r0.apk
 
 find . -name \*.apk -exec tar --strip-components=1 -xvf {} --exclude=.PKGINFO --exclude=.SIGN\* \;
+
+wget https://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/1.3.43/GraphicsMagick-1.3.43.tar.xz
+unxz GraphicsMagick-1.3.43.tar.xz
+tar xvf GraphicsMagick-1.3.43.tar
+
+cd GraphicsMagick-1.3.43
+./configure --prefix ~/.local
+make
+make install
